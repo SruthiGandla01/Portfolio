49d6ef430630b5b3892dd2d126bfffbf07a0b6d0
Sruthi Gandla <sruthigandla01@gmail.com>
2025-05-02 21:25:31 -0400

    Added Navigation to this  portfolio Git Repo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ import logo from "../Assets/logo1.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
-import { ImBlog } from "react-icons/im";
 import {
   AiFillStar,
   AiOutlineHome,
@@ -107,7 +106,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/SruthiGandla01/Portfolio/tree/master"
                 target="_blank"
                 className="fork-btn-inner"
               >
